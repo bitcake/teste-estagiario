479f982e0ac3835a7018d8864448ad056794b72e
Matheus Lessa <matheuslessarodrigues@gmail.com>
2018-03-12 21:51:11 -0300

    ok

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,28 +10,36 @@ public sealed class PlayerController : MonoBehaviour
 	public LayerMask groundLayer;
 	public float groundRaycastDistance = 3.0f;
 
+	public bool debugIsGrounded;
+	public Vector2 debugMoveDirection;
+
 	private void Update()
 	{
+		debugIsGrounded = Physics2D.Raycast( playerRigidbody.position, downDirection, groundRaycastDistance, groundLayer.value ).transform != null;
+
 		if( Input.GetButtonDown( "Jump" ) )
 		{
 			var hit = Physics2D.Raycast( playerRigidbody.position, downDirection, groundRaycastDistance, groundLayer.value );
 			if( hit.transform != null )
+			{
 				playerRigidbody.AddForce( downDirection * ( -jumpImpulse ), ForceMode2D.Impulse );
+			}
 		}
 	}
 
 	private void FixedUpdate()
 	{
 		float move = Input.GetAxis( "Horizontal" );
-		downDirection.Normalize();
 
-		playerRigidbody.velocity = downDirection * move;
+		var moveDirection = new Vector2( -downDirection.y, downDirection.x );
+		debugMoveDirection = moveDirection;
+
+		playerRigidbody.AddForce( moveDirection * move, ForceMode2D.Impulse );
 	}
 
 	private void OnDrawGizmos()
 	{
 		Gizmos.color = Color.green;
-		Vector3 gravityDirection = Quaternion.Euler( 0.0f, 0.0f, playerRigidbody.rotation ) * Vector3.down;
-		Gizmos.DrawLine( transform.position, transform.position + gravityDirection * groundRaycastDistance );
+		Gizmos.DrawLine( transform.position, transform.position + ( Vector3 ) downDirection * groundRaycastDistance );
 	}
 }
--- Assets/Scripts/PlayerGravity.cs
+++ Assets/Scripts/PlayerGravity.cs
@@ -4,6 +4,7 @@ public sealed class PlayerGravity : MonoBehaviour
 {
 	public PlayerController controller;
 	public Rigidbody2D playerRigidbody;
+	public Transform pivotTransform;
 	public Planet planet;
 
 	private void FixedUpdate()
@@ -11,8 +12,10 @@ public sealed class PlayerGravity : MonoBehaviour
 		var gravityVector = planet.GetGravityVector( playerRigidbody.position );
 
 		playerRigidbody.AddForce( gravityVector );
-		//playerRigidbody.rotation = Vector2.SignedAngle( Vector2.down, gravityVector );
 
-		controller.downDirection = new Vector2( -gravityVector.y, gravityVector.x ).normalized;
+		//playerRigidbody.rotation = Vector2.SignedAngle( Vector2.down, gravityVector );
+		pivotTransform.rotation = Quaternion.Euler( 0.0f, 0.0f, Vector2.SignedAngle( Vector2.down, gravityVector ) );
+
+		controller.downDirection = gravityVector.normalized;
 	}
 }
